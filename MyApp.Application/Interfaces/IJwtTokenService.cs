d73aa601dae86a0a2b99fda7b8082ab3edd7c79a
adnane <mohamed.adnane8@gmail.com>
2026-03-18 17:49:53 +0000

    remove unimplemented logic

--- MyApp.Application/Interfaces/IJwtTokenService.cs
+++
@@ -1,8 +0,0 @@
-using MyApp.Domain.Entities;
-
-namespace MyApp.Application.Interfaces;
-
-public interface IJwtTokenService
-{
-    string GenerateToken(User user);
-}
--- MyApp.Infrastructure/DependencyInjection.cs
+++ MyApp.Infrastructure/DependencyInjection.cs
@@ -6,7 +6,6 @@ using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using MyApp.Application.Interfaces;
-using MyApp.Domain.Entities;
 using MyApp.Infrastructure.Authentication;
 using MyApp.Infrastructure.Data;
 using MyApp.Infrastructure.Repositories;
@@ -72,32 +71,7 @@ public static class DependencyInjection
         // Authentication services
         services.AddScoped<IGoogleTokenValidator, GoogleTokenValidator>();
         services.AddScoped<ITokenService, TokenService>();
-        // Note: IJwtTokenService is replaced by ITokenService
-        // Remove registration for IJwtTokenService if not used elsewhere
-        services.AddScoped<IJwtTokenService, JwtTokenServiceAdapter>();
 
         return services;
-    }
-}
-
-/// <summary>
-/// Adapter to maintain backward compatibility with IJwtTokenService
-/// </summary>
-public class JwtTokenServiceAdapter : IJwtTokenService
-{
-    private readonly ITokenService _tokenService;
-
-    public JwtTokenServiceAdapter(ITokenService tokenService)
-    {
-        _tokenService = tokenService;
-    }
-
-    public string GenerateToken(User user)
-    {
-        // Generate a token without session ID for backward compatibility
-        // This is a simplified version - in production, migrate all usages to ITokenService
-        throw new NotImplementedException(
-            "Use ITokenService.GenerateAccessToken with session ID instead. " +
-            "This method is deprecated for security reasons.");
     }
 }
